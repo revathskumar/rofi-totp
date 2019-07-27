450f54c84a15ce2973a8461c3f9f7e9283af2f6a
Revath S Kumar <rsk@revathskumar.com>
2019-07-27 20:41:52 +0530

    fix(totp): handle space in secret

--- src/totp.rs
+++ src/totp.rs
@@ -4,7 +4,7 @@ extern crate oath;
 // use oath::totp;
 
 pub fn generate(secret: &str) -> Result<u64, &'static str> {
-  let secret_bytes = try!(base32::decode(base32::Alphabet::RFC4648 {padding: false}, secret).ok_or("invalid base32"));
+  let secret_bytes = try!(base32::decode(base32::Alphabet::RFC4648 {padding: false}, &secret.replace(" ", "")).ok_or("invalid base32"));
   let code: u64 = oath::totp_raw_now(&secret_bytes, 6, 0, 30, &oath::HashType::SHA1);
   Ok(code)
 }
